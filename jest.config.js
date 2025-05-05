798e83f89993fde681d2998be9327e6104f07ed0
sangwookpark <sangwookp9591@gmail.com>
2025-05-05 20:40:16 +0900

    feat : 파일이름 regex

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
-  },
+    testEnvironment: 'node',
+    transform: {
+        '^.+.tsx?$': ['ts-jest', {}],
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
 };
