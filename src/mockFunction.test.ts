35ddfee237acfb6485db231ac78fb2a9a8c87412
sangwookpark <sangwookp9591@gmail.com>
2025-05-21 11:28:53 +0900

    test : spy 초기화 함수, mockClear, mockReset, mockRestore

--- src/mockFunction.test.ts
+++ src/mockFunction.test.ts
@@ -1,7 +1,17 @@
 import { obj } from './mockFunction';
-
+/**
+ * spy 는 유지되기때문에 이건 자바스크립트라서 위에서 아래로 순차적으로 실행되기 때문에
+ * 아래에서  toHaveBeenCalledTimes 는 위의 obj 호출과 같이 중복되기 때문에 에러가 발생
+ * 개별적으로 하면 에러가 발생하지 않음.
+ *
+ * 그래서 spy를 없애야함. 없애는 방법이 3가지가 있음.
+ *
+ *  1. mockClear() , times, with 초기화 spy함수가 몇 번 실행됐는지 누구하 함께 호출되었는지 , calledWith같은 애들
+ *  2. mockReset() , mockClear mockImplemntation(()=>{}) , 빈함수로 만들어버림
+ *  3. mockRestore() // 아예 전부 없애버림
+ */
 test('obj.minus가 스파이를 심고 실행도 안되게', () => {
-    jest.spyOn(obj, 'minus').mockImplementation(); //mockImplementation 빈값을 입력해는것
+    const spyFn = jest.spyOn(obj, 'minus').mockImplementation(); //mockImplementation 빈값을 입력해는것
     /**
      * mockImplementation안에는 이렇게 생겼음.
      * mockImplementation(()=>{})
@@ -11,21 +21,25 @@ test('obj.minus가 스파이를 심고 실행도 안되게', () => {
     console.log(obj.minus);
     expect(obj.minus).toHaveBeenCalledTimes(1);
     expect(result).not.toBe(1);
+
+    spyFn.mockRestore();
 });
 
 test('obj.minus가 스파이를 심고 리턴값을 바꾸게', () => {
     //가짜함수를 만들어서 그런행동을 못하게 봐꿈.
-    jest.spyOn(obj, 'minus').mockImplementation((a, b) => a + b); //mockImplementation 함수의 결과를 5로 갈아끼움
+    const spyFn = jest.spyOn(obj, 'minus').mockImplementation((a, b) => a + b); //mockImplementation 함수의 결과를 5로 갈아끼움
     const result = obj.minus(2, 1);
     console.log(obj.minus);
     expect(obj.minus).toHaveBeenCalledTimes(1);
     expect(result).toBe(5);
     //결과가 2-1인 1로 나와야하는데 5로나온게 success가 됨
+    spyFn.mockClear();
 });
 
 test('obj.minus가 스파이를 심고 리턴값을 바꾸게 mockImplementationOnce', () => {
     //가짜함수를 만들어서 그런행동을 못하게 봐꿈.
-    jest.spyOn(obj, 'minus')
+    const spyFn = jest
+        .spyOn(obj, 'minus')
         .mockImplementationOnce((a, b) => a + b) //mockImplementationOnce 한번만 가짜행동을 한다.
         .mockImplementationOnce(() => 5);
     //첫 번째 실행하면 once, 두번째 실행하면 .mockImplementation(() => 5), 세번째 실행하면 원래함수로 돌아옴.
@@ -37,10 +51,11 @@ test('obj.minus가 스파이를 심고 리턴값을 바꾸게 mockImplementation
     expect(result1).toBe(3);
     expect(result2).toBe(5);
     expect(result3).toBe(1);
+    spyFn.mockRestore();
 });
 
 test('obj.minus가 스파이를 심고 리턴값이 다르게 나오게 mockReturnValue', () => {
-    jest.spyOn(obj, 'minus').mockReturnValueOnce(3).mockReturnValue(5); //함수를 바꿔끼기 보다는 리턴값만 바꿈
+    const spyFn = jest.spyOn(obj, 'minus').mockReturnValueOnce(3).mockReturnValue(5); //함수를 바꿔끼기 보다는 리턴값만 바꿈
 
     const result1 = obj.minus(2, 1);
     const result2 = obj.minus(2, 1);
@@ -50,4 +65,5 @@ test('obj.minus가 스파이를 심고 리턴값이 다르게 나오게 mockRetu
     expect(result1).toBe(3);
     expect(result2).toBe(5);
     expect(result3).toBe(1);
+    spyFn.mockRestore();
 });
