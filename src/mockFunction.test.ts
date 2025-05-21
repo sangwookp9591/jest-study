b0a47334c9a34f03bb2dab7defa39c496b929791
sangwookpark <sangwookp9591@gmail.com>
2025-05-21 11:51:08 +0900

    feat : beforeEach, afterEach등 테스트의 그룹화를 위한 describe

--- src/mockFunction.test.ts
+++ src/mockFunction.test.ts
@@ -11,30 +11,42 @@ import { obj } from './mockFunction';
  *  3. mockRestore() // 아예 전부 없애버림
  */
 let spyFn;
-test('obj.minus가 스파이를 심고 실행도 안되게', () => {
-    spyFn = jest.spyOn(obj, 'minus').mockImplementation(); //mockImplementation 빈값을 입력해는것
-    /**
-     * mockImplementation안에는 이렇게 생겼음.
-     * mockImplementation(()=>{})
-     * obj.minus라는 메서드를 () => {} 빈함수로 갈아끼우는거임
-     */
-    const result = obj.minus(2, 1);
-    console.log(obj.minus);
-    expect(obj.minus).toHaveBeenCalledTimes(1);
-    expect(result).not.toBe(1);
+let beforeEachCount = 0;
+let afterEachCount = 0;
+describe('beforeEach/ afterEach 적용', () => {
+    beforeEach(() => {
+        console.log('beforeEach ', beforeEachCount++);
+    }); //테스트 전에 뭐 변수 초기화 같은거
+    //매번 실행 후에
+    afterEach(() => {
+        console.log('afterEach ', afterEachCount++);
+        jest.restoreAllMocks();
+    }); // 정리할때 mockRestore() 같은거
+    test('obj.minus가 스파이를 심고 실행도 안되게', () => {
+        spyFn = jest.spyOn(obj, 'minus').mockImplementation(); //mockImplementation 빈값을 입력해는것
+        /**
+         * mockImplementation안에는 이렇게 생겼음.
+         * mockImplementation(()=>{})
+         * obj.minus라는 메서드를 () => {} 빈함수로 갈아끼우는거임
+         */
+        const result = obj.minus(2, 1);
+        console.log(obj.minus);
+        expect(obj.minus).toHaveBeenCalledTimes(1);
+        expect(result).not.toBe(1);
 
-    spyFn.mockRestore();
-});
+        // spyFn.mockRestore();
+    });
 
-test('obj.minus가 스파이를 심고 리턴값을 바꾸게', () => {
-    //가짜함수를 만들어서 그런행동을 못하게 봐꿈.
-    spyFn = jest.spyOn(obj, 'minus').mockImplementation((a, b) => a + b); //mockImplementation 함수의 결과를 5로 갈아끼움
-    const result = obj.minus(2, 1);
-    console.log(obj.minus);
-    expect(obj.minus).toHaveBeenCalledTimes(1);
-    expect(result).toBe(5);
-    //결과가 2-1인 1로 나와야하는데 5로나온게 success가 됨
-    spyFn.mockClear();
+    test('obj.minus가 스파이를 심고 리턴값을 바꾸게', () => {
+        //가짜함수를 만들어서 그런행동을 못하게 봐꿈.
+        spyFn = jest.spyOn(obj, 'minus').mockImplementation((a, b) => a + b); //mockImplementation 함수의 결과를 5로 갈아끼움
+        const result = obj.minus(2, 1);
+        console.log(obj.minus);
+        expect(obj.minus).toHaveBeenCalledTimes(1);
+        expect(result).toBe(5);
+        //결과가 2-1인 1로 나와야하는데 5로나온게 success가 됨
+        // spyFn.mockClear();
+    });
 });
 
 test('obj.minus가 스파이를 심고 리턴값을 바꾸게 mockImplementationOnce', () => {
@@ -52,7 +64,7 @@ test('obj.minus가 스파이를 심고 리턴값을 바꾸게 mockImplementation
     expect(result1).toBe(3);
     expect(result2).toBe(5);
     expect(result3).toBe(1);
-    spyFn.mockRestore();
+    // spyFn.mockRestore();
 });
 
 test('obj.minus가 스파이를 심고 리턴값이 다르게 나오게 mockReturnValue', () => {
@@ -66,20 +78,20 @@ test('obj.minus가 스파이를 심고 리턴값이 다르게 나오게 mockRetu
     expect(result1).toBe(3);
     expect(result2).toBe(5);
     expect(result3).toBe(1);
-    spyFn.mockRestore();
+    // spyFn.mockRestore();
 });
 
 //파일단위
 //모든 테스트 실행전에
 beforeAll(() => {}); // 데이터 베이스 연결 같은거?
 // 매번 실행 전에
-beforeEach(() => {}); //테스트 전에 뭐 변수 초기화 같은거
-//매번 실행 후에
-afterEach(() => {
-    // spyFn.mockRestore();
-    // jest.clearAllMocks(); //위처럼 안해도되게 제스트 자체에서 모든거를 클리어할수있는게 있음. 역할은 같음.
-    // jest.resetAllMocks();
-    jest.restoreAllMocks();
-}); // 정리할때 mockRestore() 같은거
+// beforeEach(() => {}); //테스트 전에 뭐 변수 초기화 같은거
+// //매번 실행 후에
+// afterEach(() => {
+//     // spyFn.mockRestore();
+//     // jest.clearAllMocks(); //위처럼 안해도되게 제스트 자체에서 모든거를 클리어할수있는게 있음. 역할은 같음.
+//     // jest.resetAllMocks();
+//     jest.restoreAllMocks();
+// }); // 정리할때 mockRestore() 같은거
 //모든 테스트 실행 후에
 afterAll(() => {}); // 모든 테스트가 끝난 후에 beforeAll에서 한거 정리하는 용도로 많이 사용
