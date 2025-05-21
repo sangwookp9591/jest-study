dfba936b979041cff4b214a284d7baf038bb8fd5
sangwookpark <sangwookp9591@gmail.com>
2025-05-21 12:20:57 +0900

    test : runAllTimers 로 시간 빨리 감기

--- src/callbak.test.ts
+++ src/callbak.test.ts
@@ -14,3 +14,13 @@ test('타이머 잘 실행되나? 2', (done) => {
     });
     //done이라는 함수 매개변수가 있는데 이렇게 테스트를 멈추고 싶을 때 done을 넣어야지만 callback함수를 알수있느음.
 });
+
+test('시간아 빨리가라!', (done) => {
+    //jest에서는 5초를 테스트가 넘기면 에러가 나버림 이럴때 runAllTimers 시간을빨리감아서 문제해결
+    jest.useFakeTimers();
+    timer((message: string) => {
+        expect(message).toBe('success');
+    });
+
+    jest.runAllTimers();
+});
